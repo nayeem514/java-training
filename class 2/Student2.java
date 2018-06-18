845a8d9f5a04b6ef56e9b1c69f379a9211e7794d
nayeem514 <nayeemakram72@gmail.com>
2018-06-18 03:16:28 -0700

    files commited

---
+++ class 2/Student2.java
@@ -0,0 +1,6 @@
+class Student2{
+public static void main(String args[])
+{
+System.out.print("Hello from Md. Nayeem");
+}
+}
---
+++ class 3/Student4.java
@@ -0,0 +1,9 @@
+class Student{
+public static void main(String args[])
+int year=2010;
+if(year%4==0){
+System.out.print("leap year")
+else(year%100!=0)
+}
+}
+}
